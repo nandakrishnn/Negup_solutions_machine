796db0d412285f50a343d49123ba86357de1d7de
nandakrishnn <unandakrishnan@gmail.com>
2024-11-05 16:11:13 +0530

    basics started

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color darkGrey=Color.fromRGBO(48, 48, 48, 1.0);
+  static const Color blueColor=Color.fromRGBO(33, 150, 243, 1.0);
+  static const Color yellowColor= Color.fromRGBO(255, 193, 7, 1.0);
+   static const Color greenColor= Color.fromRGBO(76, 175, 80, 1.0);
+    static const Color redColor=  Color.fromRGBO(244, 67, 54, 1.0);
+    static const Color bgColor=Color.fromRGBO(238, 238, 238, 1.0);
+}
